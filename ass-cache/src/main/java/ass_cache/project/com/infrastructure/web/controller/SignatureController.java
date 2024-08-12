03cc856ceed234f61a60d0ee494bff42ae8279b2
Thiago Goulart <thiago.goulart001@edu.pucrs.br>
2024-08-11 23:58:31 -0300

    creating the connections to the eureka server creating the application.yml to the gateway creating the dockerfiles and the docker compose

--- ass-cache/src/main/java/ass_cache/project/com/infrastructure/web/controller/SignatureController.java
+++ ass-cache/src/main/java/ass_cache/project/com/infrastructure/web/controller/SignatureController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/servcad")
+@RequestMapping("")
 public class SignatureController {
 
     @Autowired
--- name-server/src/main/java/name_server/project/com/NameServerApplication.java
+++ name-server/src/main/java/name_server/project/com/NameServerApplication.java
@@ -2,12 +2,12 @@ package name_server.project.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NameServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NameServerApplication.class, args);
 	}
-
 }
